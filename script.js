c2d95069478326b71c476491fea8e7c5813195dc
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-05-29 01:59:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+   arr.sort();
+	let totalCost=0;
+	while(arr.length>1){
+		let a=arr.shift();
+		let b=arr.shift();
+		totalCost+=a+b;
+		arr.push(a+b);
+		arr.sort();
+	}
+	return totalCost;
+	}
   
-}
+
 
 module.exports=mincost;
